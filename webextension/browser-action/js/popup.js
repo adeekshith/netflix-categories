8aba70ce20d0c6abe8e12b5c89ccb5bc7b8ffe70
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-08-31 17:45:19 -0500

    Add Search Field And Sample Button To Browser Action Popup

---
+++ webextension/browser-action/js/popup.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Deekshith Allamaneni on 11/28/15.
+ * Copyright 2016 Deekshith Allamaneni
+ */
+
+"use strict";
+
+function getDefPrefsRestorePopupOptions () {
+    textFileLoad(chrome.extension.getURL("data/user-preferences-default.json")).then(function(response) {
+        // The first runs when the promise resolves, with the request.reponse
+        // specified within the resolve() method.
+        restorePopupOptions(response);
+        // The second runs when the promise
+        // is rejected, and logs the Error specified with the reject() method.
+    }, function(Error) {
+        console.log(Error);
+    });
+}
+
+
+function restorePopupOptions (userPrefDefaultJsonStr) {
+    // Read from saved preferences and restore options.
+    //$('#toggle-enable-dov').bootstrapToggle('off');
+    chrome.storage.local.get({
+        user_config: userPrefDefaultJsonStr
+    }, function (items) {
+        let thisUserPreferences = JSON.parse(items.user_config);
+        let thisUserConfig = new UserConfig(thisUserPreferences);
+        //var thisUserIsDovIconEnabled = thisUserConfig.isIconBesideDocLinksEnabled();
+        if (thisUserConfig.isExtensionEnabled() === true) {
+            $('#toggle-enable-dov').bootstrapToggle('on');
+        } else {
+            $('#toggle-enable-dov').bootstrapToggle('off');
+        }
+    });
+}
+
+// document.addEventListener('DOMContentLoaded', getDefPrefsRestorePopupOptions);
