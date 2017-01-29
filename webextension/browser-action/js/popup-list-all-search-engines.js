e378e0e1d957a2531c0194b29f3d3dbd30dd9399
Deekshith Allamaneni <dkh.hyd@gmail.com>
2017-01-29 17:36:22 -0600

    Remove Category Accordion And Just Display List Nodes Instead

--- webextension/browser-action/js/popup-list-all-search-engines.js
+++ webextension/browser-action/js/popup-list-all-search-engines.js
@@ -62,48 +62,9 @@ function restoreListAllSearchEnginesPopupOptions (thisUserConfig) {
             },"");
         }
 
-        function generateSearchEngineCategoryListNodes(category) {
-            let collapseID = `search-category-${generateUuid()}`;
-            let categoryHtml = `
-            <div class="panel panel-default">
-                    <div class="list-group status search-list-category">
-                        <a class="list-group-item list-group-item-info" data-toggle="collapse" data-parent="#accordion" href="#${collapseID}">
-                            <span class="glyphicon glyphicon-chevron-right category-list-menu-collapse-indicator pull-right" aria-hidden="true"></span>${category}
-                        </a>
-                    </div>
-
-                <div id="${collapseID}" class="panel-collapse collapse">
-                    <div class="list-group">
-                        ${generateSearchEngineListNodes(thisUserConfig.getSearchEnginesByCategory(category))}
-                    </div>
-                </div>
-            </div>
-            `;
-
-            return categoryHtml;
-        }
-
-        let categoryNodes = thisUserConfig.getSearchEngineCategories()
-            .reduce((previousHtml, eachSearchCategory) => {
-                return previousHtml+generateSearchEngineCategoryListNodes(eachSearchCategory);
-            }, "");
-        document.getElementById("accordion").innerHTML=categoryNodes;
+        document.getElementById("accordion").innerHTML= generateSearchEngineListNodes(thisUserConfig.getSearchEnginesByCategory("NetflixGenre"));
     document.getElementById("accordion").addEventListener("click", processSearchEngineButtonClick);
 
-    /**
-     * Highlight Search Engine Category Item On Click
-     */
-    $('.search-list-category').on('click','> a', function(e) {
-        var $this = $(this);
-        let wasAlreadyActive = e.target.classList.contains("active");
-        $('.search-list-category').find('.active').find('.category-list-menu-collapse-indicator').removeClass('glyphicon-chevron-down');
-        $('.search-list-category').find('.active').find('.category-list-menu-collapse-indicator').addClass('glyphicon-chevron-right');
-        $('.search-list-category').find('.active').removeClass('active');
-        if (!wasAlreadyActive) {
-            $this.addClass('active');
-            $this.find('.category-list-menu-collapse-indicator').addClass('glyphicon-chevron-down');
-        }
-    });
 }
 
 document.addEventListener('DOMContentLoaded', getDefPrefsRestorePopupListAllSearchEnginesOptions);
